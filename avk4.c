910d6642944819382325665d314b164dac97741f
amruta544 <kamtheavadhoot@gmail.com>
2026-08-30 21:39:28 +0530

    Add files via upload
    Add c file

---
+++ avk4.c
@@ -0,0 +1,14 @@
+/*
+Program(4)→Write a program to calculate the area of a circle.
+Solution(2)→user define values
+*/
+#include<stdio.h>
+int main()
+{
+    float radius,area;
+    printf("\n Enter the radius of the circle:");
+    scanf("%f",&radius);
+    area=(3.14*radius*radius);
+    printf("\n Area of circle=%f",area);
+    return 0;
+}
