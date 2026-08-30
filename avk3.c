a725938e3a8461416b03efaa887e9caaabaad776
amruta544 <kamtheavadhoot@gmail.com>
2026-08-30 21:39:01 +0530

    Add files via upload
    Add c file

---
+++ avk3.c
@@ -0,0 +1,19 @@
+/*
+Program(3)→Write a program to calculate the area of circle
+Solution(1)→Predifined values
+*/
+#include<stdio.h>
+int main()
+{
+    int rollnum;
+    float per;
+    char grade;
+    rollnum=55;
+    per=88.88;
+    grade='A';
+    printf("----Student Information----\n");
+    printf("\n Roll Number:%d",rollnum);
+    printf("\n Percentage:%f",per);
+    printf("\n Grade:%c",grade);
+    return 0;
+}
